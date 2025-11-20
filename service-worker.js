e26dd88a33571020746eafee28cf0d1226d9cd36
leogammora-ux <leogammora@gmail.com>
2025-11-20 08:24:35 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => clients.claim());
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
